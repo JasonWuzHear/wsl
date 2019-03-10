e0b1a5066d22c0c643ad8be9b7f966512b93b1f7
Jason Elie Bou Kheir <jason.boukheir@pm.me>
2019-03-09 20:34:20 -0800

    made it so docker-compsoe setup install python3

--- docker-compose-setup.sh
+++ docker-compose-setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Install Python and PIP
-sudo apt install -y python-dev python-pip
+sudo apt install -y python3-dev python3-pip
 
 # Install Docker Compose into your user's home directory.
-pip install --user docker-compose
+pip3 install --user docker-compose
